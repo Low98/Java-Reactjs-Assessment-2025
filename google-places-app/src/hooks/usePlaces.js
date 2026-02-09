883c27a637b9a5c433d18117533c8f06700254e7
Low98 <83057834+Low98@users.noreply.github.com>
2026-02-09 20:37:13 +0800

    Delete google-places-app directory

--- google-places-app/src/hooks/usePlaces.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-export default function usePlaces() {
-  return {
-    searches: useSelector(state => state.places.searches),
-    favourites: useSelector(state => state.places.favourites)
-  };
-}
